da91a6c67351591762eb0a62f5cc2fed4d593b88
shangwenhe <shangwenhe@vipkid.com.cn>
2020-08-25 16:03:19 +0800

    vue.config

--- template/vue.config.js
+++ template/vue.config.js
@@ -8,10 +8,35 @@
 
 const path = require('path');
 let { fullPath, externals } = require('./aone.config.js')
+const jsonFormat = require("json-format")
+const fs = require('fs-extra')
 
 module.exports = {
   publicPath: (process.env.NODE_ENV === 'production' )? `//s.vipkidstatic.com/beeschool/${fullPath}`: '/' ,
   outputDir: path.resolve(__dirname, 'dist'),
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        let parameters = args[0]['templateParameters'];
+        function getAsset (compilation, assets, pluginOptions){
+          let filePath = config.output.store.get('path')
+          fs.exists(filePath, function(ex){
+            fs.mkdir(filePath, { recursive: true }, (err) => {
+              fs.writeFile(filePath + '/depends.map.json', jsonFormat(assets, { type: 'space', size: 2 }), function (error, data) {
+                if(error){
+                  console.log(error)
+                  process.exit(1);
+                }
+              })
+            });
+          });
+          return parameters(compilation, assets, pluginOptions);
+        }
+        args[0]['templateParameters'] = getAsset;
+        return args
+      });
+  },
   pluginOptions: {
     externals: {
       common: Object.entries(externals).map(([key, item])=>{
